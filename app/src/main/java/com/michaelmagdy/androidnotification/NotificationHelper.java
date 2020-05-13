2681ea4b32820e3c6b7d0790d62e1d07b8ff9239
Michael <michaelmagdy_93@yahoo.com>
2020-05-13 22:50:44 +0200

    handle notification onclick using pendingIntent

--- app/src/main/java/com/michaelmagdy/androidnotification/NotificationHelper.java
+++ app/src/main/java/com/michaelmagdy/androidnotification/NotificationHelper.java
@@ -1,6 +1,8 @@
 package com.michaelmagdy.androidnotification;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -11,13 +13,19 @@ public class NotificationHelper {
 
     public static void displayNotification(Context context, String title, String body){
 
+        Intent intent = new Intent(context, ProfileActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                100, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
         //notification builder
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context, CHANNEL_ID)
                         .setSmallIcon(R.drawable.ic_notifications_active_red_a700_24dp)
                         .setContentTitle(title)
                         .setContentText(body)
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
 
         //notification manager
         NotificationManagerCompat mNotificationMgr = NotificationManagerCompat.from(context);
